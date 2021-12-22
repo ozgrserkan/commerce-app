ab74906c2ec5c3ec639047f481ae16e59d4774c9
serkan <ozgr.serkan09@gmail.com>
2021-12-22 17:49:38 +0300

    admin dashboard added

---
+++ admin/src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDX09czirYZAgOMSwnqAVdDWKEbQBdXasA",
+  authDomain: "commerce-app-cbe3b.firebaseapp.com",
+  projectId: "commerce-app-cbe3b",
+  storageBucket: "commerce-app-cbe3b.appspot.com",
+  messagingSenderId: "105501794798",
+  appId: "1:105501794798:web:276bce38f32731fe85aeff",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
